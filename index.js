2b6bd71dcc6f635ba4fd93ff1d7574e571d5f251
AllisonAnz <AllisonAnzalone@gmail.com>
2021-05-02 16:32:31 -0500

    commit tree 36006c6d3acf4023ab8a0f81bd04658d55cfc34d

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// index.js: Should change the text on the page when the DOM content has loaded
+    document.addEventListener("DOMContentLoaded", function() {
+        updateDOM()
+    });
+
+    function updateDOM() {
+        document.getElementById("text").innerHTML = "This is really cool!";
+    }
+
+// The text node hold the text "JavaScript is so cool. It lets me add text to my page programmatically"
